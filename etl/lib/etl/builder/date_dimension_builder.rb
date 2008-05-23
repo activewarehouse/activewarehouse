255dfcee6d5ae6782c07c9752fea651c7e1958b2
Jacob Maine <jacob.maine@gmail.com>
2008-05-23 10:04:09 -0500

    factor out building date record from building all date records
    Should make addition of new date fields easier, by subclassing
    and overriding:
    
    class MyDateBuilder < ETL::Builder::DateDimensionBuilder
      private
    
      def record_from_date(date)
        record = super(date)
        record[:is_cinco_de_mayo] = date.month == 5 && date.day == 5
        record
      end
    end

--- etl/lib/etl/builder/date_dimension_builder.rb
+++ etl/lib/etl/builder/date_dimension_builder.rb
@@ -26,60 +26,65 @@ module ETL #:nodoc:
         @holiday_indicators = []
       end
       
-      # Returns an array of hashes representing records in the dimension. The values for each record are 
-      # accessed by name.
+      # Returns an array of hashes representing records in the dimension.
       def build(options={})
-        (start_date..end_date).map do |date|
-          time = date.to_time # need methods only available in Time
-          record = {}
-          record[:date] = time.strftime("%m/%d/%Y")
-          record[:full_date_description] = time.strftime("%B %d,%Y")
-          record[:day_of_week] = time.strftime("%A")
-          #record[:day_number_in_epoch] = time.to_i / 24
-          #record[:week_number_in_epoch] = time.to_i / (24 * 7)
-          #record[:month_number_in_epoch] = time.to_i / (24 * 7 * 30)
-          record[:day_number_in_calendar_month] = time.day
-          record[:day_number_in_calendar_year] = time.yday
-          record[:day_number_in_fiscal_month] = time.day # should this be different from CY?
-          record[:day_number_in_fiscal_year] = time.fiscal_year_yday
-          #record[:last_day_in_week_indicator] = 
-          #record[:last_day_in_month_indicator] =
-          #record[:calendar_week_ending_date] = 
-          record[:calendar_week] = "Week #{time.week}"
-          record[:calendar_week_number] = time.week
-          record[:calendar_week_number_in_year] = time.week
-          record[:calendar_month_name] = time.strftime("%B")
-          record[:calendar_month_number] = time.month
-          record[:calendar_month_number_in_year] = time.month
-          record[:calendar_year_month] = time.strftime("%Y-%m")
-          record[:calendar_quarter] = "Q#{time.quarter}"
-          record[:calendar_quarter_number] = time.quarter
-          record[:calendar_quarter_number_in_year] = time.quarter
-          record[:calendar_year_quarter] = "#{time.strftime('%Y')}-#{record[:calendar_quarter]}"
-          #record[:calendar_half_year] = 
-          record[:calendar_year] = "#{time.year}"
-          record[:fiscal_week] = "FY Week #{time.fiscal_year_week}"
-          record[:fiscal_week_number] = time.fiscal_year_week
-          record[:fiscal_week_number_in_year] = time.fiscal_year_week
-          record[:fiscal_month] = time.fiscal_year_month
-          record[:fiscal_month_number] = time.fiscal_year_month
-          record[:fiscal_month_number_in_year] = time.fiscal_year_month
-          record[:fiscal_year_month] = "FY#{time.fiscal_year}-" + time.fiscal_year_month.to_s.rjust(2, '0')
-          record[:fiscal_quarter] = "FY Q#{time.fiscal_year_quarter}"
-          record[:fiscal_year_quarter] = "FY#{time.fiscal_year}-Q#{time.fiscal_year_quarter}"
-          record[:fiscal_quarter_number] = time.fiscal_year_quarter
-          record[:fiscal_year_quarter_number] = time.fiscal_year_quarter
-          #record[:fiscal_half_year] = 
-          record[:fiscal_year] = "FY#{time.fiscal_year}"
-          record[:fiscal_year_number] = time.fiscal_year
-          record[:holiday_indicator] = holiday_indicators.include?(date) ? 'Holiday' : 'Nonholiday'
-          record[:weekday_indicator] = weekday_indicators[time.wday]
-          record[:selling_season] = 'None'
-          record[:major_event] = 'None'
-          record[:sql_date_stamp] = date
-          
-          record
-        end
+        (start_date..end_date).map { |date| record_from_date(date) }
+      end
+      
+      private
+      
+      # Returns a hash representing a record in the dimension. The values for each record are 
+      # accessed by name.
+      def record_from_date(date)
+        time = date.to_time # need methods only available in Time
+        record = {}
+        record[:date] = time.strftime("%m/%d/%Y")
+        record[:full_date_description] = time.strftime("%B %d,%Y")
+        record[:day_of_week] = time.strftime("%A")
+        #record[:day_number_in_epoch] = time.to_i / 24
+        #record[:week_number_in_epoch] = time.to_i / (24 * 7)
+        #record[:month_number_in_epoch] = time.to_i / (24 * 7 * 30)
+        record[:day_number_in_calendar_month] = time.day
+        record[:day_number_in_calendar_year] = time.yday
+        record[:day_number_in_fiscal_month] = time.day # should this be different from CY?
+        record[:day_number_in_fiscal_year] = time.fiscal_year_yday
+        #record[:last_day_in_week_indicator] = 
+        #record[:last_day_in_month_indicator] =
+        #record[:calendar_week_ending_date] = 
+        record[:calendar_week] = "Week #{time.week}"
+        record[:calendar_week_number] = time.week
+        record[:calendar_week_number_in_year] = time.week
+        record[:calendar_month_name] = time.strftime("%B")
+        record[:calendar_month_number] = time.month
+        record[:calendar_month_number_in_year] = time.month
+        record[:calendar_year_month] = time.strftime("%Y-%m")
+        record[:calendar_quarter] = "Q#{time.quarter}"
+        record[:calendar_quarter_number] = time.quarter
+        record[:calendar_quarter_number_in_year] = time.quarter
+        record[:calendar_year_quarter] = "#{time.strftime('%Y')}-#{record[:calendar_quarter]}"
+        #record[:calendar_half_year] = 
+        record[:calendar_year] = "#{time.year}"
+        record[:fiscal_week] = "FY Week #{time.fiscal_year_week}"
+        record[:fiscal_week_number] = time.fiscal_year_week
+        record[:fiscal_week_number_in_year] = time.fiscal_year_week
+        record[:fiscal_month] = time.fiscal_year_month
+        record[:fiscal_month_number] = time.fiscal_year_month
+        record[:fiscal_month_number_in_year] = time.fiscal_year_month
+        record[:fiscal_year_month] = "FY#{time.fiscal_year}-" + time.fiscal_year_month.to_s.rjust(2, '0')
+        record[:fiscal_quarter] = "FY Q#{time.fiscal_year_quarter}"
+        record[:fiscal_year_quarter] = "FY#{time.fiscal_year}-Q#{time.fiscal_year_quarter}"
+        record[:fiscal_quarter_number] = time.fiscal_year_quarter
+        record[:fiscal_year_quarter_number] = time.fiscal_year_quarter
+        #record[:fiscal_half_year] = 
+        record[:fiscal_year] = "FY#{time.fiscal_year}"
+        record[:fiscal_year_number] = time.fiscal_year
+        record[:holiday_indicator] = holiday_indicators.include?(date) ? 'Holiday' : 'Nonholiday'
+        record[:weekday_indicator] = weekday_indicators[time.wday]
+        record[:selling_season] = 'None'
+        record[:major_event] = 'None'
+        record[:sql_date_stamp] = date
+        
+        record
       end
     end
   end
