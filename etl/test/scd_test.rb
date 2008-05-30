9021790bded0805a22045691c3418ea41f55d6af
Jacob Maine <jacob.maine@gmail.com>
2008-05-30 09:37:51 -0500

    scd tests much more granular
    Not changing tests, just making them more explicit about
    what they're testing.

--- etl/test/scd_test.rb
+++ etl/test/scd_test.rb
@@ -4,61 +4,124 @@ class ScdTest < Test::Unit::TestCase
   def setup
     @connection = ETL::Engine.connection(:data_warehouse)
     @connection.delete("DELETE FROM person_dimension")
+    
+    @end_of_time = '9999-12-31 00:00:00'
   end
-  def test_type_1_scd_inserts_record
+  
+  def test_type_1_run_1_inserts_record
     do_type_1_run(1)
-    # TODO: This is a test bug - if the tests don't run at the correct
-    # time, this timestamp may not match the timestamp used during the
-    # creation of the SCD row
-    timestamp = Time.now
-    
-    lines = lines_for('scd_test_type_1.txt')
-    assert_equal(
-      "1,Bob,Smith,200 South Drive,Boston,MA,32123,#{timestamp.to_s(:db)},9999-12-31 00:00:00\n",
-      lines.first
-    )
+    assert_equal 1, count_bobs
   end
   
-  def test_type_1_scd_inserts_updated_record
+  def test_type_1_run_1_sets_original_address
+    do_type_1_run(1)
+    assert_boston_address(find_bobs.first)
+  end
+  
+  def test_type_1_run_1_sets_original_id
+    do_type_1_run(1)
+    assert_equal 1, find_bobs.first.id
+  end
+  
+  def test_type_1_run_2_deletes_old_record
+    # TODO: make this pass
+    # do_type_1_run(1)
+    # do_type_1_run(2)
+    # assert_equal 1, count_bobs, "new record created, but old not deleted: #{find_bobs.inspect}"
+  end
+  
+  def test_type_1_run_2_updates_address
+    do_type_1_run(1)
     do_type_1_run(2)
-    # TODO: This is a test bug - if the tests don't run at the correct
-    # time, this timestamp may not match the timestamp used during the
-    # creation of the SCD row
-    timestamp = Time.now
     
-    lines = lines_for('scd_test_type_1.txt')
-    assert_equal(
-      "1,Bob,Smith,1010 SW 23rd St,Los Angeles,CA,90392,#{timestamp.to_s(:db)},9999-12-31 00:00:00\n",
-      lines.first
-    )
+    assert_los_angeles_address(find_bobs.last)
   end
   
-  def test_type_2_scd
+  def test_type_1_run_2_keeps_id
+    # TODO: make this pass
+    # do_type_1_run(1)
+    # do_type_1_run(2)
+    # assert_equal 1, find_bobs.first.id
+  end
+  
+  def test_type_2_run_1_inserts_record
     do_type_2_run(1)
-    lines = lines_for('scd_test_type_2.txt')
-    
+    assert_equal 1, count_bobs
+  end
+  
+  def test_type_2_run_1_sets_original_address
+    do_type_2_run(1)
+    assert_boston_address(find_bobs.first)
+  end
+  
+  def test_type_2_run_1_sets_original_id
+    do_type_2_run(1)
+    assert_equal 1, find_bobs.first.id
+  end
+  
+  def test_type_2_run_1_sets_effective_date
+    do_type_2_run(1)
     # TODO: This is a test bug - if the tests don't run at the correct
     # time, this timestamp may not match the timestamp used during the
     # creation of the SCD row
-    timestamp = Time.now
-    assert_equal(
-      "1,Bob,Smith,200 South Drive,Boston,MA,32123,#{timestamp.to_s(:db)},9999-12-31 00:00:00\n", 
-      lines.first, "assertion failed in run 1"
-    )
-    
+    assert_equal Time.now.to_s(:db), find_bobs.first.effective_date, "failure might be a test bug - see test notes"
+  end
+  
+  def test_type_2_run_1_sets_end_date
+    do_type_2_run(1)
+    assert_equal @end_of_time, find_bobs.first.end_date
+  end
+
+  def test_type_2_run_2_inserts_new_record
+    do_type_2_run(1)
     do_type_2_run(2)
-    lines = lines_for('scd_test_type_2.txt')
-    
-    assert_equal(
-      "1,Bob,Smith,200 South Drive,Boston,MA,32123,#{timestamp.to_s(:db)},#{timestamp.to_s(:db)}\n", 
-      lines[0], "assertion failed in run 2"
-    )
-    assert_equal(
-      "2,Bob,Smith,1010 SW 23rd St,Los Angeles,CA,90392,#{timestamp.to_s(:db)},9999-12-31 00:00:00\n", 
-      lines[1]
-    )
-    
     assert_equal 2, count_bobs
+  end
+  
+  def test_type_2_run_2_keeps_id
+    do_type_2_run(1)
+    do_type_2_run(2)
+    assert_equal [1, 2], find_bobs.map(&:id).sort
+  end
+  
+  def test_type_2_run_2_expires_old_record
+    do_type_2_run(1)
+    do_type_2_run(2)
+
+    # TODO: This is a test bug - if the tests don't run at the correct
+    # time, this timestamp may not match the timestamp used during the
+    # creation of the SCD row
+    assert_equal Time.now.to_s(:db), find_bobs.detect { |bob| 1 == bob.id }.end_date, "failure might be a test bug - see test notes"
+  end
+  
+  def test_type_2_run_2_keeps_address_for_expired_record
+    do_type_2_run(1)
+    do_type_2_run(2)
+
+    assert_boston_address(find_bobs.detect { |bob| 1 == bob.id })
+  end
+  
+  def test_type_2_run_2_updates_address_on_new_record    
+    do_type_2_run(1)
+    do_type_2_run(2)
+
+    assert_los_angeles_address(find_bobs.detect { |bob| 2 == bob.id })
+  end
+  
+  def test_type_2_run_2_activates_new_record
+    do_type_2_run(1)
+    do_type_2_run(2)
+
+    # TODO: This is a test bug - if the tests don't run at the correct
+    # time, this timestamp may not match the timestamp used during the
+    # creation of the SCD row
+    assert_equal Time.now.to_s(:db), find_bobs.detect { |bob| 2 == bob.id }.effective_date, "failure might be a test bug - see test notes"    
+  end
+  
+  def test_type_2_run_2_activates_sets_end_date_for_new_record
+    do_type_2_run(1)
+    do_type_2_run(2)
+    assert_equal @end_of_time, find_bobs.detect { |bob| 2 == bob.id }.end_date
   end
   
   # # This should pass, but doesn't (b/c CRC isn't being saved?) (run outside of a job)
@@ -103,5 +166,36 @@ class ScdTest < Test::Unit::TestCase
   def count_bobs
     @connection.select_value(
       "SELECT count(*) FROM person_dimension WHERE first_name = 'Bob' and last_name = 'Smith'").to_i
+  end
+  
+  def find_bobs
+    bobs = @connection.select_all(
+      "SELECT * FROM person_dimension WHERE first_name = 'Bob' and last_name = 'Smith'")
+    bobs.each do |bob|
+      def bob.id
+        self["id"].to_i
+      end
+      def bob.effective_date
+        self["effective_date"]
+      end
+      def bob.end_date
+        self["end_date"]
+      end
+    end
+    bobs
+  end
+  
+  def assert_boston_address(bob)
+    assert_equal "200 South Drive", bob['address'], "expected Boston street: #{bob.inspect}"
+    assert_equal "Boston", bob['city'], "expected Boston city: #{bob.inspect}"
+    assert_equal "MA", bob['state'], "expected Boston state: #{bob.inspect}"
+    assert_equal "32123", bob['zip_code'], "expected Boston zip: #{bob.inspect}"
+  end
+  
+  def assert_los_angeles_address(bob)
+    assert_equal "1010 SW 23rd St", bob['address'], "expected Los Angeles street: #{bob.inspect}"
+    assert_equal "Los Angeles", bob['city'], "expected Los Angeles city: #{bob.inspect}"
+    assert_equal "CA", bob['state'], "expected Los Angeles state: #{bob.inspect}"
+    assert_equal "90392", bob['zip_code'], "expected Los Angeles zip: #{bob.inspect}"    
   end
 end
--- etl/test/scd_test_type_1.ctl
+++ etl/test/scd_test_type_1.ctl
@@ -14,7 +14,7 @@ source :in, {
 # NOTE: These are not usually required for a type 1 SCD dimension, but since
 # we're sharing this dimension with the type 2 tests, they're necessary.
 transform :effective_date, :default, :default_value => Time.now.to_s(:db)
-transform :expiration_date, :default, :default_value => '9999-12-31 00:00:00'
+transform :end_date, :default, :default_value => '9999-12-31 00:00:00'
 
 destination :out, {
   :file => 'output/scd_test_type_1.txt',
@@ -25,7 +25,7 @@ destination :out, {
 {
   :order => [
     :id, :first_name, :last_name, :address, :city, :state, :zip_code,
-    :effective_date, :expiration_date
+    :effective_date, :end_date
   ],
   :virtual => {
     :id => ETL::Generator::SurrogateKeyGenerator.new(:target => :data_warehouse, :table => 'person_dimension')
