a8752440fbe84abf0af6085fca0340c18f815a47
Jacob Maine <jacob.maine@gmail.com>
2008-10-19 16:43:38 -0700

    re-adding changes reverted by bfa28ed6b532d96841688be6d53b4202eab519d8

--- etl/lib/etl/builder/date_dimension_builder.rb
+++ etl/lib/etl/builder/date_dimension_builder.rb
@@ -24,69 +24,72 @@ module ETL #:nodoc:
       # * <tt>start_date</tt>: The start date. Defaults to 5 years ago from today.
       # * <tt>end_date</tt>: The end date. Defaults to now.
       def initialize(start_date=Time.now.years_ago(5), end_date=Time.now, fiscal_year_offset_month=10)
-        @start_date = start_date
-        @end_date = end_date
+        @start_date = start_date.to_date
+        @end_date = end_date.to_date
         @fiscal_year_offset_month = fiscal_year_offset_month.to_i
         @holiday_indicators = []
       end
       
-      # Returns an array of hashes representing records in the dimension. The values for each record are 
-      # accessed by name.
+      # Returns an array of hashes representing records in the dimension.
       def build(options={})
-        records = []
-        date = start_date.to_time
-        while date <= end_date.to_time
-          record = {}
-          record[:date] = date.strftime("%m/%d/%Y")
-          record[:full_date_description] = date.strftime("%B %d,%Y")
-          record[:day_of_week] = date.strftime("%A")
-          record[:day_in_week] = record[:day_of_week] # alias
-          #record[:day_number_in_epoch] = date.to_i / 24
-          #record[:week_number_in_epoch] = date.to_i / (24 * 7)
-          #record[:month_number_in_epoch] = date.to_i / (24 * 7 * 30)
-          record[:day_number_in_calendar_month] = date.day
-          record[:day_number_in_calendar_year] = date.yday
-          record[:day_number_in_fiscal_month] = date.day # should this be different from CY?
-          record[:day_number_in_fiscal_year] = date.fiscal_year_yday(fiscal_year_offset_month)
-          #record[:last_day_in_week_indicator] = 
-          #record[:last_day_in_month_indicator] =
-          #record[:calendar_week_ending_date] = 
-          record[:calendar_week] = "Week #{date.week}"
-          record[:calendar_week_number] = date.week
-          record[:calendar_week_number_in_year] = date.week # DEPRECATED
-          record[:calendar_month_name] = date.strftime("%B")
-          record[:calendar_month_number_in_year] = date.month # DEPRECATED
-          record[:calendar_month_number] = date.month
-          record[:calendar_year_month] = date.strftime("%Y-%m")
-          record[:calendar_quarter] = "Q#{date.quarter}"
-          record[:calendar_quarter_number] = date.quarter
-          record[:calendar_quarter_number_in_year] = date.quarter # DEPRECATED
-          record[:calendar_year_quarter] = "#{date.strftime('%Y')}-#{record[:calendar_quarter]}"
-          #record[:calendar_half_year] = 
-          record[:calendar_year] = "#{date.year}"
-          record[:fiscal_week] = "FY Week #{date.fiscal_year_week(fiscal_year_offset_month)}"
-          record[:fiscal_week_number_in_year] = date.fiscal_year_week(fiscal_year_offset_month) # DEPRECATED
-          record[:fiscal_week_number] = date.fiscal_year_week(fiscal_year_offset_month)
-          record[:fiscal_month] = date.fiscal_year_month(fiscal_year_offset_month)
-          record[:fiscal_month_number] = date.fiscal_year_month(fiscal_year_offset_month)
-          record[:fiscal_month_number_in_year] = date.fiscal_year_month(fiscal_year_offset_month) # DEPRECATED
-          record[:fiscal_year_month] = "FY#{date.fiscal_year(fiscal_year_offset_month)}-" + date.fiscal_year_month(fiscal_year_offset_month).to_s.rjust(2, '0')
-          record[:fiscal_quarter] = "FY Q#{date.fiscal_year_quarter(fiscal_year_offset_month)}"
-          record[:fiscal_year_quarter] = "FY#{date.fiscal_year(fiscal_year_offset_month)}-Q#{date.fiscal_year_quarter(fiscal_year_offset_month)}"
-          record[:fiscal_year_quarter_number] = date.fiscal_year_quarter(fiscal_year_offset_month)
-          #record[:fiscal_half_year] = 
-          record[:fiscal_year] = "FY#{date.fiscal_year(fiscal_year_offset_month)}"
-          record[:fiscal_year_number] = date.fiscal_year(fiscal_year_offset_month)
-          record[:holiday_indicator] = holiday_indicators.include?(date) ? 'Holiday' : 'Nonholiday'
-          record[:weekday_indicator] = weekday_indicators[date.wday]
-          record[:selling_season] = 'None'
-          record[:major_event] = 'None'
-          record[:sql_date_stamp] = date
-          
-          records << record
-          date = date.tomorrow
-        end
-        records
+        (start_date..end_date).map { |date| record_from_date(date) }
+      end
+
+      private
+
+      # Returns a hash representing a record in the dimension. The values for each record are 
+      # accessed by name.
+      def record_from_date(date)
+        time = date.to_time # need methods only available in Time
+        record = {}
+        record[:date] = time.strftime("%m/%d/%Y")
+        record[:full_date_description] = time.strftime("%B %d,%Y")
+        record[:day_of_week] = time.strftime("%A")
+        record[:day_in_week] = record[:day_of_week] # alias
+        #record[:day_number_in_epoch] = time.to_i / 24
+        #record[:week_number_in_epoch] = time.to_i / (24 * 7)
+        #record[:month_number_in_epoch] = time.to_i / (24 * 7 * 30)
+        record[:day_number_in_calendar_month] = time.day
+        record[:day_number_in_calendar_year] = time.yday
+        record[:day_number_in_fiscal_month] = time.day # should this be different from CY?
+        record[:day_number_in_fiscal_year] = time.fiscal_year_yday(fiscal_year_offset_month)
+        #record[:last_day_in_week_indicator] = 
+        #record[:last_day_in_month_indicator] =
+        #record[:calendar_week_ending_date] = 
+        record[:calendar_week] = "Week #{time.week}"
+        record[:calendar_week_number] = time.week
+        record[:calendar_week_number_in_year] = time.week # DEPRECATED
+        record[:calendar_month_name] = time.strftime("%B")
+        record[:calendar_month_number_in_year] = time.month # DEPRECATED
+        record[:calendar_month_number] = time.month
+        record[:calendar_year_month] = time.strftime("%Y-%m")
+        record[:calendar_quarter] = "Q#{time.quarter}"
+        record[:calendar_quarter_number] = time.quarter
+        record[:calendar_quarter_number_in_year] = time.quarter # DEPRECATED
+        record[:calendar_year_quarter] = "#{time.strftime('%Y')}-#{record[:calendar_quarter]}"
+        #record[:calendar_half_year] = 
+        record[:calendar_year] = "#{time.year}"
+        record[:fiscal_week] = "FY Week #{time.fiscal_year_week(fiscal_year_offset_month)}"
+        record[:fiscal_week_number_in_year] = time.fiscal_year_week(fiscal_year_offset_month) # DEPRECATED
+        record[:fiscal_week_number] = time.fiscal_year_week(fiscal_year_offset_month)
+        record[:fiscal_month] = time.fiscal_year_month(fiscal_year_offset_month)
+        record[:fiscal_month_number] = time.fiscal_year_month(fiscal_year_offset_month)
+        record[:fiscal_month_number_in_year] = time.fiscal_year_month(fiscal_year_offset_month) # DEPRECATED
+        record[:fiscal_year_month] = "FY#{time.fiscal_year(fiscal_year_offset_month)}-" + time.fiscal_year_month(fiscal_year_offset_month).to_s.rjust(2, '0')
+        record[:fiscal_quarter] = "FY Q#{time.fiscal_year_quarter(fiscal_year_offset_month)}"
+        record[:fiscal_year_quarter] = "FY#{time.fiscal_year(fiscal_year_offset_month)}-Q#{time.fiscal_year_quarter(fiscal_year_offset_month)}"
+        record[:fiscal_quarter_number] = time.fiscal_year_quarter(fiscal_year_offset_month) # DEPRECATED
+        record[:fiscal_year_quarter_number] = time.fiscal_year_quarter(fiscal_year_offset_month)
+        #record[:fiscal_half_year] = 
+        record[:fiscal_year] = "FY#{time.fiscal_year(fiscal_year_offset_month)}"
+        record[:fiscal_year_number] = time.fiscal_year(fiscal_year_offset_month)
+        record[:holiday_indicator] = holiday_indicators.include?(date) ? 'Holiday' : 'Nonholiday'
+        record[:weekday_indicator] = weekday_indicators[time.wday]
+        record[:selling_season] = 'None'
+        record[:major_event] = 'None'
+        record[:sql_date_stamp] = date
+
+        record
       end
     end
   end
