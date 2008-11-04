10aac5be84663bc1297e5db1fd6c79ce81137686
Anthony Eden <anthonyeden@gmail.com>
2008-11-04 16:00:01 -0500

    moved etl to new repo: http://github.com/aeden/activewarehouse-etl

--- etl/lib/etl/execution/batch.rb
+++
@@ -1,8 +0,0 @@
-module ETL #:nodoc:
-  module Execution #:nodoc:
-    # Persistent class representing an ETL batch
-    class Batch < Base
-      has_many :jobs
-    end
-  end
-end
--- etl/lib/etl/version.rb
+++
@@ -1,9 +0,0 @@
-module ETL#:nodoc:
-  module VERSION #:nodoc:
-    MAJOR = 0
-    MINOR = 9
-    TINY  = 1
-
-    STRING = [MAJOR, MINOR, TINY].join('.')
-  end
-end
