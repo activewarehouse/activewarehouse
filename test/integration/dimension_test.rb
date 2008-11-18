60f1f24e86c9862a4541d8103f0e8b3c1a29d539
Anthony Eden <anthonyeden@gmail.com>
2008-11-18 14:20:35 -0500

    make setup run on integration tests

--- test/integration/dimension_test.rb
+++ test/integration/dimension_test.rb
@@ -1,18 +1,19 @@
-require "#{File.dirname(__FILE__)}/../test_helper"
+require "#{File.dirname(__FILE__)}/test_helper"
 
 class DimensionTest < Test::Unit::TestCase
   context "the Store dimension" do
     setup do
-      StoreDimension.create!(:store_state => 'Florida')
-      StoreDimension.create!(:store_state => 'Florida')
-      StoreDimension.create!(:store_state => 'Florida')
-      StoreDimension.create!(:store_state => 'Georgia')
-      StoreDimension.create!(:store_state => 'Alabama')
-      StoreDimension.create!(:store_state => 'Alabama')
+      StoreDimension.create!(:store_state => 'Florida', :store_region => 'South')
+      StoreDimension.create!(:store_state => 'Florida', :store_region => 'South')
+      StoreDimension.create!(:store_state => 'Florida', :store_region => 'South')
+      StoreDimension.create!(:store_state => 'Georgia', :store_region => 'South')
+      StoreDimension.create!(:store_state => 'Alabama', :store_region => 'South')
+      StoreDimension.create!(:store_state => 'Alabama', :store_region => 'South')
     end
     context "for the available values" do
       should "return an array of unique available values" do
         assert_equal ['Alabama','Florida','Georgia'], StoreDimension.available_values(:store_state)
+        assert_equal ['South'], StoreDimension.available_values(:store_region)
       end
     end
   end
--- test/schema.rb
+++ test/schema.rb
@@ -1,6 +1,8 @@
+ActiveRecord::Migration.verbose = false
 ActiveRecord::Schema.define do
   drop_table :store_dimension
   create_table :store_dimension do |t|
+    t.string :store_region, :null => false
     t.string :store_state, :null => false
   end 
 end
