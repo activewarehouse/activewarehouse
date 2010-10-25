034ef0aad23bd826fd31b2ed436ffe0e8017350b
chrisrhoden <carhoden@gmail.com>
2010-10-25 15:35:58 -0400

    generators for rails 3

--- lib/active_warehouse/railtie.rb
+++ lib/active_warehouse/railtie.rb
@@ -3,6 +3,13 @@ module ActiveWarehouse
     # Rails-3.0.1 requires config.app_generators instead of 3.0.0's config.generators    
     rake_tasks do
       load "active_warehouse/tasks/active_warehouse.rake"
+    end
+    
+    generators do
+      Dir[File.expand_path('*.rb', File.dirname(__FILE__))].each do |file|
+        require file
+      end
+    end
   end
 end
   
