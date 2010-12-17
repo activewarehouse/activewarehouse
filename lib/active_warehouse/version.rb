4ee2ac6ffe5b82b92245acdd8029f7e64b8276d1
kookster <andrew@beginsinwonder.com>
2010-12-17 18:38:35 -0500

    fix error message on reload of gem for previously defined constants

--- lib/active_warehouse/version.rb
+++ lib/active_warehouse/version.rb
@@ -1,9 +1,13 @@
 module ActiveWarehouse #:nodoc:
   module VERSION #:nodoc:
-    MAJOR = 0
-    MINOR = 4
-    TINY  = 0
+    
+    unless defined? STRING
+      MAJOR = 0
+      MINOR = 4
+      TINY  = 0
 
-    STRING = [MAJOR, MINOR, TINY].join('.')
+      STRING = [MAJOR, MINOR, TINY].join('.')
+    end
+
   end
 end
