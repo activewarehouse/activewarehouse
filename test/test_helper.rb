c961e07b1ec901eb0279e5d89f3770f8111bf27a
Thibaut Barrère <thibaut.barrere@gmail.com>
2012-04-22 22:58:00 +0200

    rake test should work now

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,8 +8,15 @@ require 'activewarehouse'
 raise "Missing required DB environment variable" unless ENV['DB']
 
 require 'etl'
+
+database_yml = File.dirname(__FILE__) + '/config/database.yml'
+ETL::Engine.init(:config => database_yml)
+
 ETL::Engine.logger = Logger.new('etl.log')
 ETL::Engine.logger.level = Logger::ERROR
+
+
+ActiveRecord::Base.establish_connection(:awunit)
 
 require 'setup'
 require 'populate'
@@ -20,4 +27,4 @@ require 'standard_aggregation_assertions'
 require 'hierarchical_dimension_aggregation_assertions'
 require 'hierarchical_slowly_changing_dimension_aggregation_assertions'
 
-ActiveRecord::Base.logger.level = Logger::DEBUG
+#ActiveRecord::Base.logger.level = Logger::DEBUG
